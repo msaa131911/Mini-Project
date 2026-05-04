6a86f10a249b6e6981611e66f3ea14cacc54c0f8
MSAA <msaa131911@gmail.com>
2026-05-04 22:55:06 +0600

    Update ML pipeline.py

--- ML pipeline.py
+++ ML pipeline.py
@@ -1,4 +1,3 @@
-#Built ML pipeline using feature selection (OMP) and Kernel Ridge Regression for regression task
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -6,54 +5,50 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import OrthogonalMatchingPursuit
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class OMPFeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, n_nonzero_coefs=5):
+        self.n_nonzero_coefs = n_nonzero_coefs
+
+    def fit(self, X, y):
+        self.model = OrthogonalMatchingPursuit(n_nonzero_coefs=self.n_nonzero_coefs)
+        self.model.fit(X, y)
+        self.selected_idx = np.where(self.model.coef_ != 0)[0]
+        return self
+
+    def transform(self, X):
+     X= X.to_numpy()
+     return X[:, self.selected_idx]
 
 
 df = pd.read_csv(r"C:\Users\MS AA\Downloads\train (1).csv")
 
-#  only numeric
-df = df.select_dtypes(include=['int64', 'float64'])
-df = df.dropna()
+df=df.select_dtypes(include=['int64', 'float64'])
+df=df.dropna()
 
+x=df.drop("SalePrice",axis=1)
+y=df["SalePrice"]
 
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-X = df.drop("SalePrice", axis=1)
-y = df["SalePrice"]
+pipeline = Pipeline([
+    ('feature_selection', OMPFeatureSelector(n_nonzero_coefs=10)),
+    ('scaler', StandardScaler()),
+    ('kernel_ridge', KernelRidge())
+])
 
+param_grid = {
+    'feature_selection__n_nonzero_coefs': [5, 10, 15],
+    'kernel_ridge__alpha': [0.1, 1, 10],
+    'kernel_ridge__kernel': ['linear', 'rbf']
+}
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+grid = GridSearchCV(pipeline, param_grid, cv=3)
+grid.fit(X_train, y_train)
 
+print("Best Params:", grid.best_params_)
+print("R2 Score:", grid.score(X_test, y_test))
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
-
-omp = OrthogonalMatchingPursuit(n_nonzero_coefs=10)
-omp.fit(X_train_scaled, y_train)
-
-coef = pd.Series(omp.coef_, index=X.columns)
-selected_features = coef[coef != 0].index
-
-#print("\nSelected Features:\n", selected_features)
-
-# index mapping
-feature_idx = [X.columns.get_loc(col) for col in selected_features]
-
-# reduce dataset
-X_train_sel = X_train_scaled[:, feature_idx]
-X_test_sel = X_test_scaled[:, feature_idx]
-
-
-model = KernelRidge(alpha=1.0, kernel='rbf', gamma=0.1)
-model.fit(X_train_sel, y_train)
-
-
-y_pred = model.predict(X_test_sel)
-
-
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-r2 = r2_score(y_test, y_pred)
-
-
-print("RMSE:", rmse)
-print("R2 Score:", r2)
